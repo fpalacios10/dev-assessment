9415935e442a32f89f063e7071e1035413b16743
Francisco Palacios <franciscopalacios10@yahoo.com>
2016-09-01 15:41:16 -0700

    converted dev assessment from bootstrap to Foundation

---
+++ site-foundation/js/custom.js
@@ -0,0 +1,29 @@
+$(function() {
+	//	Slick carousel
+	$('.your-class').slick({
+	  infinite: true,
+	  slidesToShow: 1,
+	  slidesToScroll: 1, 
+	  autoplay: true,
+	  autoplaySpeed: 4000,
+	  prevArrow: '<i  id="leftArrow" class=" arrows fa fa-arrow-circle-left fa-3x"  aria-hidden="true"></i>',
+	  nextArrow: '<i  id="rightArrow" class=" arrows fa fa-arrow-circle-right  fa-3x"  aria-hidden="true"></i>'
+	});
+	
+  	//Change arrow size if mobile
+	var width = $(window).width();
+	if (width<=420) {
+		$('#leftArrow, #rightArrow').removeClass('fa-3x').addClass('fa-2x');
+	}
+	else {
+	}
+
+	//set weather
+	var jsonData = $.getJSON("weather-data.json", function(jsonData) {
+		console.log(jsonData); 
+		var temp = jsonData.data[0].item.condition.temp; 
+		var unit = jsonData.data[0].units.temperature;
+		var link = jsonData.data[0].link;
+		$("#weather").append('<a href="'+link+'"><i class="fa fa-cloud" aria-hidden="true"></i>' + temp +'&deg; '+ unit +'</a>');
+	});
+});
